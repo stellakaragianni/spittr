d6ce4c2c208f4cd00b29d4920333c1aee58baf6f
stellakaragianni <142885590+stellakaragianni@users.noreply.github.com>
2024-06-10 09:45:25 +0300

    Delete Spittr/src directory

--- Spittr/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public class Main {
-    public static void main(String[] args) {
-        SpittleService spittleservice = new SpittleServiceImpl();
-        Spittle aa = spittleservice.createSpittle(12L, "hello", newDate);
-    }
-}
--- Spittr/src/SpittleService.java
+++
@@ -1,7 +0,0 @@
-import java.util.Date;
-
-public interface SpittleService {
-    Spittle createSpittle(long id, String text, Date postedTime);
-    Spittle deleteSpittle(long id, Spitter spitter);
-    Spittle addSpittle(Spittle spittle, Spitter spitter);
-}
